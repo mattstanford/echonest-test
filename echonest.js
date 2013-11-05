67b829ed812105750fe2ec26352e2787fb0e1f16
Matt Stanford <mds6058@gmail.com>
2013-11-04 20:53:26 -0500

    Artist searching

--- echonest.js
+++ echonest.js
@@ -1,5 +1,7 @@
 (function() {
 
+	var echonest_api_key = "5KZM0TWV9BY8TUUGR";
+
 	$(document).ready( function() {
 		
 			$("#search-button").click(search_clicked);
@@ -8,7 +10,21 @@
 
 	function search_clicked() {
 	
-		alert("hi");
+		var echonest_url_head = "http://developer.echonest.com/api/v4/"
+		
+		get_artist_info(echonest_url_head, "radiohead");
+	}
+	
+	function get_artist_info(url_head, artist)
+	{
+		var url = url_head + "artist/search?api_key=" + echonest_api_key + "&name=" + artist;
+	
+		$.getJSON(url, get_artist_info_callback);
+	}
+	
+	function get_artist_info_callback(data)
+	{
+		console.log(data);
 	}
 
 })();
