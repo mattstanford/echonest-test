6252f99c9d516a5f8a81850f050a0193c49a453e
Matt Stanford <mds6058@gmail.com>
2013-11-04 20:40:29 -0500

    Initial commit

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ echonest-test
 =============
 
 echonest-js
+
+Test project to test out some Echo Nest API functions (http://developer.echonest.com/)
---
+++ echonest.js
@@ -0,0 +1,14 @@
+(function() {
+
+	$(document).ready( function() {
+		
+			$("#search-button").click(search_clicked);
+		
+	});
+
+	function search_clicked() {
+	
+		alert("hi");
+	}
+
+})();
